f70855cd6d3093a130c07a8ea8c827a25105b994
FotoVerite <mzbphoto@gmail.com>
2015-02-24 18:26:47 +0700

    fixes double quote.

--- app/assets/javascripts/staff.js
+++ app/assets/javascripts/staff.js
@@ -1,6 +1,4 @@
-//= require jquery
-//= require jquery_ujs
-//= require_tree .// This is a manifest file that'll be compiled into application.js, which will include all the files
+//This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
