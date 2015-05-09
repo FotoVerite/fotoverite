43a3714472750e195a3f9cbbc79b10a6bc576927
FotoVerite <mzbphoto@gmail.com>
2015-05-09 04:07:18 -0400

    fixes for smaller galleries.

--- app/assets/javascripts/public/setup_gallery.js
+++ app/assets/javascripts/public/setup_gallery.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // or with jQuery
   $('.photo-container').justifiedGallery({
-    rowHeight: 270,
+    rowHeight: 470,
     lastRow: 'justify',
     margins: 15,
     captions: $('.portfolio-image').length > 0
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'fotoverite'
 set :repo_url, 'git@github.com:FotoVerite/fotoverite.git'
 set :scm, :git
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/downloads public/images public/system}
 
 namespace :deploy do
